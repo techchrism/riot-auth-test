455b941702b2fe243088e1002f72ab53e1adc478
techchrism <chris@techchrism.me>
2023-11-18 15:09:35 -0800

    add logging

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import winston from 'winston'
+import 'winston-daily-rotate-file'
+
+const logger = winston.createLogger({
+    level: 'silly',
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.padLevels(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.DailyRotateFile({
+            level: 'silly',
+            dirname: 'logs',
+            filename: 'log-%DATE%.log',
+            zippedArchive: true,
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json()
+            )
+        })
+    ]
+})
+
